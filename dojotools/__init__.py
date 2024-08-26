a88cb2b8dac22357eed660f325820611c655167d
itdojo <colin@itdojo.com>
2024-08-26 12:15:13 -0400

    Update __init__.py

--- dojotools/__init__.py
+++ dojotools/__init__.py
@@ -1,7 +1,6 @@
 from .drawLine import draw_line
 from .getOS import os_is
 from .macFormatter import format_mac_address
-from .macosCommands import some_macos_command_function
 from .ouiLookup import oui_lookup
 from .rootCheck import check_root
 from .wifiSelector import interface_selector
