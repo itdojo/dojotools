5645c8412557e9a4e0fda02393f2f0d0379ffc18
itdojo <colin@itdojo.com>
2025-11-17 14:19:08 -0500

    Update drawline function

---
+++ tools/drawline.py
@@ -0,0 +1,66 @@
+"""
+This module provides a function to draw decorative lines in the terminal.
+
+The function 'draw_line' prints a line composed of a specific character, 
+spanning the entire width of the terminal window. It leverages the 
+os.get_terminal_size method to dynamically determine the width of the 
+terminal at runtime, ensuring that the printed line always fits perfectly 
+across the terminal window, regardless of its current size.
+
+Functions:
+    draw_line(linetype=1): Prints a line of a specified type across the terminal window.
+
+Available Line Types:
+    1: Solid line   (────────────────)
+    2: Dotted line  (∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙)
+    3: I-Beam line  (⌶⌶⌶⌶⌶⌶⌶⌶⌶⌶⌶⌶⌶⌶)
+    4: Star line    (☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆)
+    5: Solid Star   (✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯✯)
+    6: Double line  (⏥⏥⏥⏥⏥⏥⏥⏥⏥)
+    7: Diamond line (♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢)
+    8: Box line     (⎕⎕⎕⎕⎕⎕⎕⎕⎕⎕⎕⎕⎕⎕⎕⎕)
+    Any other value defaults to a solid line (────────────────).
+
+This is intended for use in console-based applications where visually distinct 
+sections are required for better readability or aesthetic appeal.
+
+Example:
+    >>> drawline(4)
+    This will print a line of stars spanning the width of the terminal.
+
+Returns:
+    True: Indicates successful execution and line printing.
+"""
+
+def drawline(linetype=1):
+    """
+    Prints a line composed of a specified character type, spanning the entire width of the terminal.
+
+    Parameters:
+    linetype (int, optional): Specifies the type of line to be drawn. Defaults to 1 (solid line).
+
+    Returns:
+    bool: True if the line is printed successfully.
+    """
+    terminal_size = get_terminal_size()
+    match linetype:
+        case 1:
+            linestyle = '─'  # solid line
+        case 2:
+            linestyle = '∙'  # dotted line
+        case 3:
+            linestyle = '⌶'  # I-beam line
+        case 4:
+            linestyle = '☆'  # star line (outline)
+        case 5:
+            linestyle = '✯'  # star line (solid)
+        case 6:
+            linestyle = '⏥'  # double line
+        case 7: 
+            linestyle = '♢'  # diamond line
+        case 8:
+            linestyle = '⎕'  # box line
+        case _:
+            linestyle = '─'  # sold line
+    print(f"{linestyle * terminal_size.columns}")
+    return True
